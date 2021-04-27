3b795892f5b4af8ca3e7a4804776956cb0ee4570
shawon shariff <contactshawon.hstu@gmail.com>
2021-04-27 14:46:34 +0600

    First Commit of Virex/Salah

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$('.owl-fashion').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:true,
+    autoplay:true,
+    autoplayTimeout:2500,
+    autoplaySpeed:200,
+    responsive:{
+        0:{
+            items:1
+        },
+        430:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+	});
